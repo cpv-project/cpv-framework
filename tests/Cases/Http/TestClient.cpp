2023ae0ccf45080fda0d7490ec8941918bf643f2
dudes-come <dudes@qq.com>
2018-04-04 17:07:33 +0800

    add HttpClient concurrent test

--- tests/Cases/Http/TestClient.cpp
+++ tests/Cases/Http/TestClient.cpp
@@ -1,3 +1,5 @@
+#include <core/semaphore.hh>
+#include <core/gate.hh>
 #include <CPVFramework/Http/Client.hpp>
 #include <TestUtility/GTestUtils.hpp>
 
@@ -45,14 +47,32 @@ TEST(TestHttpClient, parseResponse) {
 
 TEST_FUTURE(TestHttpClient, httpClient) {
 	// TODO: use local service
-	// TODO: add concurrent test
-	auto client = cpv::HttpClient::create("httpbin.org", 80);
-	return client.makeRequest("GET", "/anything").send(client).then([] (auto response) {
-		std::cout << response.getStatus() << std::endl;
-		for (const auto& header : response.getHeaders()) {
-			std::cout << header.first << ": " << header.second  << std::endl;
-		}
-		std::cout << response.getBody() << std::endl;
+	return seastar::do_with(
+		seastar::semaphore(20),
+		seastar::gate(),
+		100UL,
+		cpv::HttpClient::create("httpbin.org", 80),
+		[] (auto& limit, auto& gate, auto& count, auto& client) {
+		return seastar::repeat([&limit, &gate, &count, &client] {
+			return seastar::get_units(limit, 1).then([&gate, &count, &client] (auto units) {
+				gate.enter();
+				client.makeRequest("GET", "/anything").send(client).then([] (auto response) {
+					std::cout << response.getStatus() << std::endl;
+					/* for (const auto& header : response.getHeaders()) {
+						std::cout << header.first << ": " << header.second  << std::endl;
+					}
+					std::cout << response.getBody() << std::endl; */
+				}).finally([&gate, units = std::move(units)] {
+					gate.leave();
+				});
+			}).then([&count] {
+				return --count == 0 ?
+					seastar::stop_iteration::yes :
+					seastar::stop_iteration::no;
+			});
+		}).finally([&gate] {
+			return gate.close();
+		});;
 	});
 }
 
