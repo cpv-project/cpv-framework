247ede902275ac66739fe1d21e7aba51c4b404b8
compiv <compiv@tutanota.com>
2019-01-16 13:10:27 +0000

    add logging classes

---
+++ include/CPVFramework/Logging/Logger.hpp
@@ -0,0 +1,66 @@
+#pragma once
+#include <seastar/core/shared_ptr.hh>
+#include "../Utility/StringUtils.hpp"
+#include "../Utility/EnumUtils.hpp"
+#include "../Utility/Macros.hpp"
+
+namespace cpv {
+	/** For logger */
+	enum class LogLevel {
+		Emergency = 0,
+		Alert = 1,
+		Critical = 2,
+		Error = 3,
+		Warning = 4,
+		Notice = 5,
+		Info = 6,
+		Debug = 7
+	};
+
+	template <>
+	struct EnumDescriptions<LogLevel> {
+		static const std::vector<std::pair<LogLevel, const char*>>& get();
+	};
+	
+	/** Interface use to log messages */
+	class Logger {
+	public:
+		/** Get the log level */
+		LogLevel getLogLevel() const { return logLevel_; }
+
+		/** Set the log level */
+		void setLogLevel(LogLevel logLevel) { logLevel_ = logLevel; }
+
+		/** Check whether this log level is enabled */
+		bool isEnabled(LogLevel logLevel) const { return logLevel <= logLevel_; }
+
+		/** Log message with specified log level, it may do nothing if the level is not enabled */
+		template <class... Args>
+		void log(LogLevel logLevel, const Args&... args) {
+			if (CPV_UNLIKELY(isEnabled(logLevel))) {
+				logImpl(logLevel, joinString("",
+					"<CPV:", logLevel, "> ", joinString(" ", args...), '\n'));
+			}
+		}
+
+		/** Constructor */
+		explicit Logger(LogLevel logLevel);
+
+		/** Virtual destructor */
+		virtual ~Logger() = default;
+
+		/** Create a console logger */
+		static seastar::shared_ptr<Logger> createConsole(LogLevel logLevel);
+
+		/** Create a noop logger */
+		static seastar::shared_ptr<Logger> createNoop();
+
+	protected:
+		/** The implmentation of log, may write to console or database */
+		virtual void logImpl(LogLevel logLevel, const std::string& message) = 0;
+
+	private:
+		LogLevel logLevel_;
+	};
+}
+
---
+++ src/Logging/ConsoleLogger.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "./ConsoleLogger.hpp"
+
+namespace cpv {
+	namespace {
+		/** Colorize log output */
+		static const char* getColorCodePrefix(LogLevel logLevel) {
+			if (logLevel <= LogLevel::Error) {
+				return "\e[31m"; // red
+			} else if (logLevel<= LogLevel::Notice) {
+				return "\e[33m"; // yellow
+			} else if (logLevel >= LogLevel::Debug) {
+				return "\e[90m"; // gray
+			} else {
+				return "";
+			}
+		}
+		
+		/** Reset default color */
+		static const char* getColorCodeSuffix() {
+			return "\e[0m";
+		}
+	}
+	
+	/** The implmentation of log, writes to console */
+	void ConsoleLogger::logImpl(LogLevel logLevel, const std::string& message) {
+		if (logLevel <= LogLevel::Warning) {
+			std::cerr << getColorCodePrefix(logLevel) << message << getColorCodeSuffix();
+		} else {
+			std::cout << getColorCodePrefix(logLevel) << message << getColorCodeSuffix();
+		}
+	}
+}
+
