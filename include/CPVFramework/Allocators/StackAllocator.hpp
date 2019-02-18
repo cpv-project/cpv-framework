e0b9ab7a283f3906fbf4da74e566c27a55c096c6
compiv <compiv@tutanota.com>
2019-02-18 12:16:26 +0000

    update stack allocated containers, support copy and move assignment

--- include/CPVFramework/Allocators/StackAllocator.hpp
+++ include/CPVFramework/Allocators/StackAllocator.hpp
@@ -10,13 +10,29 @@
 namespace cpv {
 	/**
 	 * Allocator that allocate initial memory from stack (or the space contains this allocator).
-	 * Notice it doesn't support move construction, use it in stl containers will remove fast move support,
-	 * and the move consturctor is deleted to avoid memory allocate from allocator A deallocate from B.
+	 * Notice this implementation is hacky and doesn't follow the standard allocator requirement.
+	 *
+	 * STL containers think allocators are just proxy types and the memory resource can be shared
+	 * between multiple allocators, so memory allocated from allocator A can be deallocated from
+	 * allocator B if A is copied to B or A is copied to C then to B, that make lifetime management
+	 * of the memory resource much much harder, allocators should keep reference or shared pointer
+	 * to the memory resource to arrive this requirement.
+	 *
+	 * For stack allocator share the storage with multiple allocators is just bad, sure I can make
+	 * the storage external and require user to reserve it then pass to the allocator, but that will
+	 * make copy or move container dangerous, and since some container will rebind the allocator for
+	 * internal data type, the initial size is very hard to calculate, for example if you use 1024
+	 * as initial size, it's not easy to calculate how many slots of hash table can reserve for
+	 * an unordered map that won't trigger allocation from upstream allocator.
+	 *
+	 * Use this allocator for STL containers require override constructors and assignment operators,
+	 * and should perform heavy tests to verify it works correctly.
 	 */
 	template <class T, std::size_t InitialSize, class UpstreamAllocator = std::allocator<T>>
 	class StackAllocator : private UpstreamAllocator {
 	private:
 		using UpstreamAllocatorTrait = std::allocator_traits<UpstreamAllocator>;
+		static_assert(std::is_same_v<T, typename UpstreamAllocatorTrait::value_type>);
 		
 	public:
 		using value_type = T;
@@ -30,18 +46,28 @@ namespace cpv {
 		T* allocate(std::size_t n) {
 			std::size_t index = index_;
 			if (CPV_LIKELY(index + n <= InitialSize)) {
+				// allocate from internal storage and increase counter
 				T* ptr = reinterpret_cast<T*>(&storage_[index]);
 				index_ = index + n;
+				++count_;
 				return ptr;
 			} else {
+				// allocate from upstream allocator
 				return UpstreamAllocatorTrait::allocate(*this, n);
 			}
 		}
 		
 		/** Deallocate memory */
 		void deallocate(T* ptr, std::size_t n) {
-			if (static_cast<void*>(ptr) < static_cast<void*>(storage_.begin()) ||
-				static_cast<void*>(ptr) >= static_cast<void*>(storage_.end())) {
+			if (CPV_LIKELY(
+				static_cast<void*>(ptr) >= static_cast<void*>(storage_.begin()) &&
+				static_cast<void*>(ptr) < static_cast<void*>(storage_.end()))) {
+				// if all pointer allocated from initial storage deallocated, reset the index
+				if (--count_ == 0) {
+					index_ = 0;
+				}
+			} else {
+				// deallocate from upstream allocator
 				return UpstreamAllocatorTrait::deallocate(*this, ptr, n);
 			}
 		}
@@ -54,14 +80,22 @@ namespace cpv {
 		/** Disable move consturctor */
 		StackAllocator(StackAllocator&&) = delete;
 		
+		/** Disable copy assignment */
+		StackAllocator& operator=(const StackAllocator&) = delete;
+		
+		/** Disable move assignment */
+		StackAllocator& operator=(StackAllocator&&) = delete;
+		
 		/** Constructor */
 		StackAllocator() :
 			storage_(),
-			index_(0) { }
+			index_(0),
+			count_(0) { }
 		
 	private:
 		std::array<std::aligned_storage_t<sizeof(T), alignof(T)>, InitialSize> storage_;
 		std::size_t index_;
+		std::size_t count_;
 	};
 	
 	/** Compare StackAllocator, always return not equal */
@@ -82,7 +116,13 @@ namespace cpv {
 		return true;
 	}
 	
-	/** std::vector uses StackAllocator, reserve initial storage automatically */
+	/**
+	 * std::vector uses StackAllocator, reserve initial storage automatically.
+	 * Size comparison on 64 bit gcc:
+	 * - std::vector<int>: 24
+	 * - StackAllocatedVector<int, 1>: 48 (24 + 4 * 1 (storage) + 4 (padding) + 8 (index) + 8 (count))
+	 * - StackAllocatedVector<int, 100>: 440 (24 + 4 * 100 (storage) + 8 (index) + 8 (count))
+	 */
 	template <class T, std::size_t InitialSize, class UpstreamAllocator = std::allocator<T>>
 	class StackAllocatedVector :
 		public std::vector<T, StackAllocator<T, InitialSize, UpstreamAllocator>> {
@@ -99,16 +139,29 @@ namespace cpv {
 				this->emplace_back(std::move(item));
 			}
 		}
+		StackAllocatedVector& operator=(const StackAllocatedVector& other) {
+			Base::operator=(other);
+			return *this;
+		}
+		StackAllocatedVector& operator=(StackAllocatedVector&& other) {
+			Base::operator=(std::move(other));
+			return *this;
+		}
 	};
 	
-	/** std::unordered_map uses StackAllocator, reserve initial hash table automatically */
+	/**
+	 * std::unordered_map uses StackAllocator, reserve initial hash table automatically.
+	 * - std::unordered_map<int, int>: 56
+	 * - StackAllocatedUnorderedMap<int, int, 1>: 88 (56 + 16 (storage) + 8 (index) + 8 (count))
+	 * - StackAllocatedUnorderedMap<int, int, 100>: 1672 (56 + 16 * 100 (storage) + 8 (index) + 8 (count))
+	 */
 	template <
 		class Key,
 		class T,
 		std::size_t InitialSize,
 		class Hash = std::hash<Key>,
 		class KeyEqual = std::equal_to<Key>,
-		class UpstreamAllocator = std::allocator<T>>
+		class UpstreamAllocator = std::allocator<std::pair<const Key, T>>>
 	class StackAllocatedUnorderedMap : public std::unordered_map<
 		Key, T, Hash, KeyEqual,
 		StackAllocator<std::pair<const Key, T>, InitialSize, UpstreamAllocator>> {
@@ -135,6 +188,14 @@ namespace cpv {
 			for (auto& item : items) {
 				this->emplace(std::move(item));
 			}
+		}
+		StackAllocatedUnorderedMap& operator=(const StackAllocatedUnorderedMap& other) {
+			Base::operator=(other);
+			return *this;
+		}
+		StackAllocatedUnorderedMap& operator=(StackAllocatedUnorderedMap&& other) {
+			Base::operator=(std::move(other));
+			return *this;
 		}
 	};
 }
--- tests/Cases/Allocators/TestStackAllocator.cpp
+++ tests/Cases/Allocators/TestStackAllocator.cpp
@@ -30,8 +30,15 @@ TEST(TestStackAllocator, vector) {
 	cpv::StackAllocatedVector<std::string, 3> vec({ "a", "b", "c" });
 	cpv::StackAllocatedVector<std::string, 3> vecCopy(vec);
 	cpv::StackAllocatedVector<std::string, 3> vecMove(std::move(vec));
+	cpv::StackAllocatedVector<std::string, 3> vecCopyAssign;
+	cpv::StackAllocatedVector<std::string, 3> vecMoveAssignFrom;
+	cpv::StackAllocatedVector<std::string, 3> vecMoveAssign;
 	cpv::StackAllocatedVector<std::string, 3> vecAppend;
+	
 	vecCopy.at(2) = "c_";
+	vecCopyAssign = vecCopy;
+	vecMoveAssignFrom = vecCopy;
+	vecMoveAssign = std::move(std::move(vecMoveAssignFrom));
 	vecAppend.emplace_back("a");
 	vecAppend.emplace_back("b");
 	vecAppend.emplace_back("c");
@@ -45,11 +52,32 @@ TEST(TestStackAllocator, vector) {
 	ASSERT_EQ(vecMove.at(0), "a");
 	ASSERT_EQ(vecMove.at(1), "b");
 	ASSERT_EQ(vecMove.at(2), "c");
+	ASSERT_EQ(vecCopyAssign.at(0), "a");
+	ASSERT_EQ(vecCopyAssign.at(1), "b");
+	ASSERT_EQ(vecCopyAssign.at(2), "c_");
+	ASSERT_TRUE(vecMoveAssignFrom.empty());
+	ASSERT_EQ(vecMoveAssign.at(0), "a");
+	ASSERT_EQ(vecMoveAssign.at(1), "b");
+	ASSERT_EQ(vecMoveAssign.at(2), "c_");
 	ASSERT_EQ(vecAppend.at(0), "a");
 	ASSERT_EQ(vecAppend.at(1), "b");
 	ASSERT_EQ(vecAppend.at(2), "c");
 	ASSERT_EQ(vecAppend.at(3), "d");
 	ASSERT_EQ(vecAppend.at(4), "e");
+}
+
+TEST(TestStackAllocator, vectorReset) {
+	cpv::StackAllocatedVector<int, 3> vec;
+	void* begin = static_cast<void*>(&vec);
+	void* end = static_cast<void*>(&vec + 1);
+	for (std::size_t i = 0; i < 5; ++i) {
+		vec = {};
+		for (std::size_t j = 0; j < 3; ++j) {
+			vec.emplace_back(j);
+			ASSERT_GE(static_cast<void*>(&vec.at(j)), begin);
+			ASSERT_LT(static_cast<void*>(&vec.at(j)), end);
+		}
+	}
 }
 
 TEST(TestStackAllocator, unorderedMap) {
@@ -61,7 +89,14 @@ TEST(TestStackAllocator, unorderedMap) {
 	cpv::StackAllocatedUnorderedMap<int, std::string, 3> mapCopy(map);
 	cpv::StackAllocatedUnorderedMap<int, std::string, 3> mapMove(std::move(map));
 	cpv::StackAllocatedUnorderedMap<int, std::string, 3> mapAppend;
+	cpv::StackAllocatedUnorderedMap<int, std::string, 3> mapCopyAssign;
+	cpv::StackAllocatedUnorderedMap<int, std::string, 3> mapMoveAssignFrom;
+	cpv::StackAllocatedUnorderedMap<int, std::string, 3> mapMoveAssign;
+	
 	mapCopy.at(102) = "c_";
+	mapCopyAssign = mapCopy;
+	mapMoveAssignFrom = mapCopy;
+	mapMoveAssign = std::move(std::move(mapMoveAssignFrom));
 	mapAppend.emplace(100, "a");
 	mapAppend.emplace(101, "b");
 	mapAppend.emplace(102, "c");
@@ -75,6 +110,13 @@ TEST(TestStackAllocator, unorderedMap) {
 	ASSERT_EQ(mapMove.at(100), "a");
 	ASSERT_EQ(mapMove.at(101), "b");
 	ASSERT_EQ(mapMove.at(102), "c");
+	ASSERT_EQ(mapCopyAssign.at(100), "a");
+	ASSERT_EQ(mapCopyAssign.at(101), "b");
+	ASSERT_EQ(mapCopyAssign.at(102), "c_");
+	ASSERT_TRUE(mapMoveAssignFrom.empty());
+	ASSERT_EQ(mapMoveAssign.at(100), "a");
+	ASSERT_EQ(mapMoveAssign.at(101), "b");
+	ASSERT_EQ(mapMoveAssign.at(102), "c_");
 	ASSERT_EQ(mapAppend.at(100), "a");
 	ASSERT_EQ(mapAppend.at(101), "b");
 	ASSERT_EQ(mapAppend.at(102), "c");
@@ -82,8 +124,22 @@ TEST(TestStackAllocator, unorderedMap) {
 	ASSERT_EQ(mapAppend.at(104), "e");
 }
 
+TEST(TestStackAllocator, unorderedMapReset) {
+	cpv::StackAllocatedUnorderedMap<int, int, 3> map;
+	void* begin = static_cast<void*>(&map);
+	void* end = static_cast<void*>(&map + 1);
+	for (std::size_t i = 0; i < 5; ++i) {
+		map = {};
+		for (std::size_t j = 0; j < 3; ++j) {
+			map.emplace(j, j);
+			ASSERT_GE(static_cast<void*>(&map.at(j)), begin);
+			ASSERT_LT(static_cast<void*>(&map.at(j)), end);
+		}
+	}
+}
+
 namespace {
-	// test different initial sizes to verify alignment
+	// test different initial sizes to verify alignment with sanitizers
 	template <std::size_t InitialSize>
 	void testStackAllocatedUnorderedMapWithInitialSize() {
 		cpv::StackAllocatedUnorderedMap<int, int, InitialSize> map;
