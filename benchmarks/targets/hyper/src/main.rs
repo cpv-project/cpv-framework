bb527c60bb502509237ff356eecd4f9124296c0e
303248153 <303248153@qq.com>
2020-01-20 14:23:05 +0000

    update benchmark code for hyper

--- benchmarks/targets/hyper/src/main.rs
+++ benchmarks/targets/hyper/src/main.rs
@@ -1,19 +1,34 @@
 use std::convert::Infallible;
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server};
+use hyper::{Body, Request, Response, Server, Method, StatusCode, header};
 
-async fn hello(_: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new(Body::from("Hello World!")))
+async fn hello(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+	match (req.method(), req.uri().path()) {
+		(&Method::GET, "/") => {
+			Ok(Response::builder()
+				.status(StatusCode::OK)
+				.header(header::CONTENT_TYPE, "text/plain")
+				.body(Body::from("Hello World!"))
+				.unwrap())
+		},
+		_ => {
+			Ok(Response::builder()
+				.status(StatusCode::NOT_FOUND)
+				.header(header::CONTENT_TYPE, "text/plain")
+				.body(Body::from("Not Found"))
+				.unwrap())
+		}
+	}
 }
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let make_svc = make_service_fn(|_conn| {
-        async { Ok::<_, Infallible>(service_fn(hello)) }
-    });
-    let addr = ([127, 0, 0, 1], 8000).into();
-    let server = Server::bind(&addr).serve(make_svc);
-    println!("Listening on http://{}", addr);
-    server.await?;
-    Ok(())
+	let make_svc = make_service_fn(|_conn| {
+		async { Ok::<_, Infallible>(service_fn(hello)) }
+	});
+	let addr = ([127, 0, 0, 1], 8000).into();
+	let server = Server::bind(&addr).serve(make_svc);
+	println!("Listening on http://{}", addr);
+	server.await?;
+	Ok(())
 }
